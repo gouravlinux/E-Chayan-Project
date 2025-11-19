be7ead52f5646aae049f0d8f82aff7ec2636f396
gouravlinux <2005gouravsoni@gmail.com>
2025-11-19 21:50:55 +0530

    settings.py line deleted to not see static files twice

--- e_chayan_project/settings.py
+++ e_chayan_project/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "votingApp" / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "votingApp" / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 STATIC_ROOT = BASE_DIR / "staticfiles"
